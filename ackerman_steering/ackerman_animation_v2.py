b8388458665e0eab48b8e594eda46fd6c9bb3e41
devilsu <devilsu@umich.edu>
2022-03-06 22:06:30 -0500

    Progress in ackerman_animation_v2.py

--- ackerman_steering/ackerman_animation_v2.py
+++ ackerman_steering/ackerman_animation_v2.py
@@ -45,8 +45,78 @@ class Ackerman:
 
         # Set font
         plt.rc('font', family='serif')  # font
-        # Create figure and axes
-        self.fig1, self.axes1=plt.subplots(1, 1, figsize=(8, 6))
+
+        #############################################
+        # Figure 1, UI interactivce stuff
+        #############################################
+        # Blank figure
+        self.fig1=plt.figure(figsize=(8,2))
+
+        # Set slider position
+        margin_left  =0.3
+        margin_bottom=0.05
+        spacing      =0.1
+
+        # Slider width and height
+        width      =0.6
+        height     =0.1
+
+        # Slider bg color
+        axcolor='lightgoldenrodyellow'
+
+        # left, bottom, width, height
+        ax_ackpoint_offset=self.fig1.add_axes([margin_left, margin_bottom,           width, height], facecolor=axcolor)
+        ax_taillen        =self.fig1.add_axes([margin_left, margin_bottom+1*spacing, width, height], facecolor=axcolor)
+        ax_stepin         =self.fig1.add_axes([margin_left, margin_bottom+2*spacing, width, height], facecolor=axcolor)
+        ax_angfrw         =self.fig1.add_axes([margin_left, margin_bottom+3*spacing, width, height], facecolor=axcolor)
+
+        self.s_ackpoint_offset=Slider(ax=ax_ackpoint_offset,
+                                      label='Ackerman point offset (mm)',
+                                      valmin=-self.paras['track'], valmax=2*self.paras['track'], valinit=0, valstep=0.1,
+                                      color=color_used[0],
+                                      orientation='horizontal')
+        
+        self.s_taillen        =Slider(ax=ax_taillen,
+                                      label='tail length (mm)',
+                                      valmin=-self.paras['wheel_base'], valmax=self.paras['wheel_base'], valinit=5, valstep=0.1,
+                                      color=color_used[1],
+                                      orientation='horizontal')
+        
+        self.s_stepin         =Slider(ax=ax_stepin,
+                                      label='stepin (mm)',
+                                      valmin=0, valmax=self.paras['track']/2, valinit=5, valstep=0.1,
+                                      color=color_used[0],
+                                      orientation='horizontal')
+        
+        self.s_angfrw         =Slider(ax=ax_angfrw,
+                                      label='RFW Angle (deg)',
+                                      valmin=-90, valmax=90, valinit=0, valstep=0.1,
+                                      color=color_used[3],
+                                      orientation='horizontal')
+
+
+        self.paras['ackpoint_offset']=self.s_ackpoint_offset.val
+        self.paras['taillen'        ]=self.s_taillen.val
+        self.paras['stepin'         ]=self.s_stepin.val
+        self.paras['angfrw'         ]=self.s_angfrw.val
+
+        # s_ackpoint_offset.on_changed(update)
+        # s_taillen.on_changed(update)
+        # s_stepin.on_changed(update)
+        # s_angfrw.on_changed(update)
+
+        # Reset button
+        ax_rst=self.fig1.add_axes([0.03, 0.85, 0.1, 0.1])
+        bt_rst=Button(ax_rst, 'Reset', color=axcolor, hovercolor='0.975')
+
+        bt_rst.on_clicked(self.reset)
+        #############################################
+
+
+        #############################################
+        # Figure 2
+        #############################################
+        self.fig2, self.axes2=plt.subplots(1, 1, figsize=(8, 6))
         # self.fig.tight_layout()
 
         # Major ticks every 20, minor ticks every 5
@@ -54,101 +124,128 @@ class Ackerman:
         minor_ticks=np.arange(-90, 91, 5)
 
         # Set major and minor ticks
-        self.axes1.set_xticks(major_ticks)
-        self.axes1.set_xticks(minor_ticks, minor=True)
-        self.axes1.set_yticks(major_ticks)
-        self.axes1.set_yticks(minor_ticks, minor=True)
+        self.axes2.set_xticks(major_ticks)
+        self.axes2.set_xticks(minor_ticks, minor=True)
+        self.axes2.set_yticks(major_ticks)
+        self.axes2.set_yticks(minor_ticks, minor=True)
 
         # Set major and minor grid lines
-        self.axes1.grid(which='major')
-        self.axes1.grid(which='minor', linestyle='--')
+        self.axes2.grid(which='major')
+        self.axes2.grid(which='minor', linestyle='--')
 
         # XY limits
-        self.axes1.set_xlim([-50, 90])
-        self.axes1.set_ylim([-90, 90])
+        self.axes2.set_xlim([-50, 90])
+        self.axes2.set_ylim([-90, 90])
 
         # XY lables
-        self.axes1.set_xlabel("Front Right Wheel Steering Angle (deg)")
-        self.axes1.set_ylabel("Front Left Wheel Steering Angle (deg)")
+        self.axes2.set_xlabel("Front Right Wheel Steering Angle (deg)")
+        self.axes2.set_ylabel("Front Left Wheel Steering Angle (deg)")
+        
         # Title
-        self.axes1.set_title("Ackerman")
+        self.axes2.set_title("Ackerman")
 
         # Horizontal line
-        self.f1_l1=self.axes1.axhline(0, color='r', )
+        self.f1_l1=self.axes2.axhline(0, color='r')
         # Vertical line
-        self.f1_l2=self.axes1.axvline(0, color='r')
-        # Infinite black line going through point a and b.
-        self.f1_l3=self.axes1.axline((0, 0), (1, 1), color='k', label='Simple')
+        self.f1_l2=self.axes2.axvline(0, color='r')
+        # Infinite black line going through point a and b
+        self.f1_l3=self.axes2.axline((0, 0), (1, 1), color='gray', label='Simple')
 
-        self.axes1.legend(handles=[self.f1_l3])
+        # Ideal
+        self.ang_ideal=self.cal_ideal_ackerman()
+        self.f1_l4,   =self.axes2.plot(self.ang_ideal[0, :], self.ang_ideal[1, :], color='k', label='Ideal')
 
+        # Real
+        # self.ang_real =self.cal_real_ackerman()
+        # self.f1_l5,   =self.axes2.plot(self.ang_real[ 0, :], self.ang_real[ 1, :], color='b', label='Real')
 
-        #############################################
-        # UI interactivce stuff
-        #############################################
-        # Blank figure
-        self.fig2=plt.figure(figsize=(8,2))
-
-        # Set slider position
-        margin_left  =0.4
-        margin_bottom=0.05
-        spacing      =0.1
-
-        # Slider width and height
-        width      =0.5
-        height     =0.1
-
-        # Slider bg color
-        axcolor='lightgoldenrodyellow'
-
-        # left, bottom, width, height
-        ax_ackpoint_offset=self.fig2.add_axes([margin_left, margin_bottom,           width, height], facecolor=axcolor)
-        ax_taillen        =self.fig2.add_axes([margin_left, margin_bottom+1*spacing, width, height], facecolor=axcolor)
-        ax_stepin         =self.fig2.add_axes([margin_left, margin_bottom+2*spacing, width, height], facecolor=axcolor)
-        ax_angfrw         =self.fig2.add_axes([margin_left, margin_bottom+3*spacing, width, height], facecolor=axcolor)
-
-        s_ackpoint_offset =Slider(ax=ax_ackpoint_offset,
-                                  label='Ackerman point offset (mm)',
-                                  valmin=-self.paras['track'], valmax=2*self.paras['track'], valinit=0, valstep=0.1,
-                                  color=color_used[0],
-                                  orientation='horizontal')
-
-        
-        s_taillen         =Slider(ax=ax_taillen,
-                                  label='tail length (mm)',
-                                  valmin=-self.paras['wheel_base'], valmax=self.paras['wheel_base'], valinit=5, valstep=0.1,
-                                  color=color_used[1],
-                                  orientation='horizontal')
-
-        
-        s_stepin          =Slider(ax=ax_stepin,
-                                  label='stepin (mm)',
-                                  valmin=0, valmax=self.paras['track']/2, valinit=5, valstep=0.1,
-                                  color=color_used[0],
-                                  orientation='horizontal')
-
-        
-        s_angfrw          =Slider(ax=ax_angfrw,
-                                  label='RFW Angle (deg)',
-                                  valmin=-90, valmax=90, valinit=0, valstep=0.1,
-                                  color=color_used[3],
-                                  orientation='horizontal')
-
-        # s_ackpoint_offset.on_changed(update)
-        # s_taillen.on_changed(update)
-        # s_stepin.on_changed(update)
-        # s_angfrw.on_changed(update)
-
-        ax_rst=self.fig2.add_axes([0.03, 0.05, 0.1, 0.03])
-        bt_rst=Button(ax_rst, 'Reset', color=axcolor, hovercolor='0.975')
-        # bt_rst.on_clicked(reset)
+        self.axes2.legend(handles=[self.f1_l3, self.f1_l4])
         #############################################
 
+        # self.fig2.canvas.mpl_connect("resize_event", self.TextResizer(lst_text))
 
+       
 
         plt.show()
 
         return
+
+    def reset(self, event):
+        self.s_stepin.reset()
+        self.s_taillen.reset()
+        self.s_ackpoint_offset.reset()
+        self.s_angfrw.reset()
+
+        self.paras['ackpoint_offset']=self.s_ackpoint_offset.val
+        self.paras['taillen'        ]=self.s_taillen.val
+        self.paras['stepin'         ]=self.s_stepin.val
+        self.paras['angfrw'         ]=self.s_angfrw.val
+
+        # ackerman.ackpoint_offset=s_ackpoint_offset.val
+        # ackerman.stepin         =s_stepin.val
+        # ackerman.taillen        =s_taillen.val
+        # ackerman.angfrw         =s_angfrw.val
+
+        return
+
+    def cal_ideal_ackerman(self):
+
+        ang_right_list=np.arange(0, 91)
+
+        # Ideal, turn right
+        ideal_ang_right_list=[]
+        ideal_ang_left_list =[]
+        for ang_right in ang_right_list:
+            
+            if ang_right>0:
+                d=self.paras['wheel_base']/np.tan(ang_right/180*pi)
+                ang_left=np.arctan(self.paras['wheel_base']/(self.paras['track']+d))
+                ang_left=ang_left/pi*180
+
+            if ang_right<0:
+                d=self.paras['wheel_base']/np.tan(-ang_right/180*pi)
+                ang_left=np.arctan(self.paras['wheel_base']/(d-self.paras['track']))
+                ang_left=-ang_left/pi*180
+            
+            if ang_right==0:
+                ang_left=0
+                ideal_ang_right_list.append(ang_right)
+                ideal_ang_left_list.append( ang_left)
+            else:
+                # Ideal, turn right
+                ideal_ang_right_list.append(ang_right)
+                ideal_ang_left_list.append( ang_left)
+
+                # Ideal, turn left
+                ideal_ang_right_list.append(-ang_left)
+                ideal_ang_left_list.append( -ang_right)
+
+        ang_ideal=np.asarray([ideal_ang_right_list, ideal_ang_left_list], dtype=np.float32)
+        ang_ideal=ang_ideal[:, ang_ideal[0, :].argsort()]
+
+        return ang_ideal
+
+    def cal_real_ackerman(self):
+
+        ang_right_list=np.arange(0, 91)
+
+        # Ideal, turn right
+        real_ang_right_list=[]
+        real_ang_left_list =[]
+        for ang_right in ang_right_list:
+            dum=1
+
+
+
+
+
+
+        ang_real=np.asarray([real_ang_right_list, real_ang_left_list], dtype=np.float32)
+        ang_real=ang_real[:, ang_real[0, :].argsort()]
+
+        return ang_real
+
+
 
 
 if __name__=="__main__":
